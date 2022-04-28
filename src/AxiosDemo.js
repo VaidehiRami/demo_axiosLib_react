4dac6e1e460cdce0e87d32524f6fc2ff85c3cb66
VaidehiRami <ramivaidehi@gmail.com>
2022-04-28 16:22:53 +0530

    Changes

--- src/AxiosDemo.js
+++ src/AxiosDemo.js
@@ -1,29 +1,27 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
+const url = 'https://jsonplaceholder.typicode.com/users';
 const AxiosDemo = () => {
   const [joke,setJoke]=useState();
 
   const fetchJoke = async () => {
     try{
-      const [data]=await axios(url,{
-        headers:{
-          Accepts:'application/json',
-        },
-      });
-      setJoke(data.joke);
+      const resp=await axios(url);
+      console.log(resp);
+     
+
     }catch(error){
-      console.log(error.response);
+      console.log(error);
     }
-    // console.log('fetch joke');
+    
   };
 
   return (
     <div>
         <span>Axios Demo..</span><br/>
-        <button onClick={fetchJoke}>Random joke</button>
-        <p >{joke}</p>
+        <button onClick={fetchJoke}>Random User</button>
+        
     </div>
   )
 }
